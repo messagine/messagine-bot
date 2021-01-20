42581fc0b8d8ed9ad45ab32cd2042abfe6618227
Harun Sokullu <harunsokullu@gmail.com>
2021-01-20 19:26:23 +0300

    update start

--- Command/StartCommand.ts
+++ Command/StartCommand.ts
@@ -2,11 +2,12 @@ import { CommandBase } from './CommandBase';
 
 export class StartCommand extends CommandBase {
   public async _execute() {
+    let startMessage: string;
     if (this.newUser) {
-      await this.bot.sendMessage(this.chatId, 'Welcome back. To find new chat, type /find_chat command.');
+      startMessage = `Welcome to Every Chat Bot. To find new chat, type /find_chat. Your language is ${this.languageCode}, to change your language type /set_language`;
     } else {
-      await this.dataHandler.addUser(this.chatId, this.languageCode);
-      await this.bot.sendMessage(this.chatId, `Welcome to Every Chat Bot. To find new chat, type /find_chat. Your language is ${this.languageCode}, to change your language type /set_language`);
+      startMessage = 'Welcome back. To find new chat, type /find_chat command.';
     }
+    await this.bot.sendMessage(this.chatId, startMessage);
   }
 }
