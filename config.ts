d0fef24f85a279ed588cbeeefb9a76954dcb8ef3
Harun Sokullu <harunsokullu@gmail.com>
2021-01-21 01:14:09 +0300

    send languages

--- config.ts
+++ config.ts
@@ -1,5 +1,6 @@
 export default {
   BOT_TOKEN: process.env.BOT_TOKEN ?? 'INVALID TOKEN',
   DB_URL: process.env.DB_URL ?? 'INVALID URL',
-  CALLBACK_JOIN: '#'
+  CALLBACK_JOIN: '#',
+  LANGUAGES_IN_ROW: 2
 }
--- src/Command/SetLanguageCommand.ts
+++ src/Command/SetLanguageCommand.ts
@@ -1,24 +1,20 @@
+import { InlineKeyboardButton } from 'node-telegram-bot-api';
+import config from '../../config';
 import { IUser } from '../models/User';
 import { CommandBase } from './CommandBase';
 
 export class SetLanguageCommand extends CommandBase {
   public async _execute(user: IUser): Promise<any> {
-    let retryMessage: string;
-    if (user.languageCode) {
-      retryMessage = `Your language is ${user.languageCode}. Choose language below.`;
-    } else {
-      retryMessage = 'Choose language below.';
-    }
+    const languages = await this.dataHandler.getLanguages();
+    const callbackJoin = config.CALLBACK_JOIN;
+    const languagesInRow = config.LANGUAGES_IN_ROW;
 
-    const options = {
-      reply_markup: {
-        inline_keyboard: [
-          [{ text: 'English', callback_data: 'LANG#en' }],
-          [{ text: 'Turkish', callback_data: 'LANG#tr' }]
-        ]
-      }
-    };
+    const inlineKeyboardItems: InlineKeyboardButton[] = languages.map(language => { return { text: language.name, callback_data: `LANG${callbackJoin}${language.lang}` }});
+    const inlineKeyboard: InlineKeyboardButton[][] = [];
+    while(inlineKeyboardItems.length) inlineKeyboard.push(inlineKeyboardItems.splice(0, languagesInRow));
 
-    await this.telegramHandler.sendMessage(this.chatId, retryMessage, options);
+    const chooseMessage = `Your language is ${user.languageCode}. Choose language below.`;
+    const options = { reply_markup: { inline_keyboard: inlineKeyboard } };
+    await this.telegramHandler.sendMessage(this.chatId, chooseMessage, options);
   }
 }
