2c7700523d886a760620234f237a77b65eedf8b8
Harun Sokullu <harunsokullu@gmail.com>
2021-01-21 22:24:41 +0300

    test

--- config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  BOT_TOKEN: process.env.BOT_TOKEN ?? 'INVALID TOKEN',
-  DB_URL: process.env.DB_URL ?? 'INVALID URL',
-  BOT_HOOK_PATH: 'https://tlpnlzb40f.execute-api.us-east-1.amazonaws.com/main',
-  CALLBACK_JOIN: '#',
-  MAC_ROWCHARS_IN_INLINE: 35,
-  DEFAULT_LANGUAGE_CODE: 'en'
-}
--- handler.js
+++ handler.js
@@ -2,17 +2,12 @@ const { Telegraf } = require('telegraf')
 // @ts-expect-error not a dependency of Telegraf
 const makeHandler = require('lambda-request-handler')
 
-const token = process.env.BOT_TOKEN
-if (token === undefined) {
-  throw new Error('BOT_TOKEN must be provided!')
-}
-
-const bot = new Telegraf(token, {
+const bot = new Telegraf(process.env.BOT_TOKEN, {
   telegram: { webhookReply: true }
 })
 
 bot.start((ctx) => ctx.reply('Hello'))
 
-export const handler = makeHandler(
-  bot.webhookCallback(process.env.BOT_HOOK_PATH ?? '/')
+export const main = makeHandler(
+  bot.webhookCallback('https://tlpnlzb40f.execute-api.us-east-1.amazonaws.com/main')
 )
