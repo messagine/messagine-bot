d9b104ee03047baf4ddfa6b0cf55afa88bb09cf8
Harun Sokullu <harunsokullu@gmail.com>
2021-01-21 19:32:27 +0300

    dynamic items in inline language

--- config.ts
+++ config.ts
@@ -2,6 +2,6 @@ export default {
   BOT_TOKEN: process.env.BOT_TOKEN ?? 'INVALID TOKEN',
   DB_URL: process.env.DB_URL ?? 'INVALID URL',
   CALLBACK_JOIN: '#',
-  LANGUAGES_IN_ROW: 2,
+  MAC_ROWCHARS_IN_INLINE: 35,
   DEFAULT_LANGUAGE_CODE: 'en'
 }
--- src/Command/SetLanguageCommand.ts
+++ src/Command/SetLanguageCommand.ts
@@ -7,11 +7,27 @@ export class SetLanguageCommand extends CommandBase {
   public async _execute(user: IUser): Promise<any> {
     const languages = await this.dataHandler.getLanguages();
     const callbackJoin = config.CALLBACK_JOIN;
-    const languagesInRow = config.LANGUAGES_IN_ROW;
+    const maxRowChars = config.MAC_ROWCHARS_IN_INLINE;
 
     const inlineKeyboardItems: InlineKeyboardButton[] = languages.map(language => { return { text: language.name, callback_data: `LANG${callbackJoin}${language.lang}` }});
     const inlineKeyboard: InlineKeyboardButton[][] = [];
-    while(inlineKeyboardItems.length) inlineKeyboard.push(inlineKeyboardItems.splice(0, languagesInRow));
+
+    let currentRow: InlineKeyboardButton[] = []
+    inlineKeyboardItems.forEach(item => {
+      if (currentRow.length > 0) {
+        const currentRowCharLength = currentRow.reduce((sum, current) => sum + current.text.length, 0);
+        const itemTextLength = item.text.length;
+        if (currentRowCharLength + itemTextLength > maxRowChars) {
+          inlineKeyboard.push(currentRow);
+          currentRow = [item];
+        } else {
+          currentRow.push(item);
+        }
+      } else {
+        currentRow.push(item);
+      }
+    });
+    if (currentRow.length > 0) inlineKeyboard.push(currentRow);
 
     const chooseMessage = `Your language is ${user.languageCode}. Choose language below.`;
     const options = { reply_markup: { inline_keyboard: inlineKeyboard } };
