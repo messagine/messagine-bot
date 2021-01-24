ee8e316efe070a294deb1d693f7abcae59f31089
Harun Sokullu <harunsokullu@gmail.com>
2021-01-24 20:21:08 +0300

    remove unnecessary configs

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,5 @@ export default {
 	ENDPOINT_URL: process.env.ENDPOINT_URL || '',
 	WEBHOOK_PATH: process.env.WEBHOOK_PATH || '',
 	IS_DEV: process.env.DEV === 'true' || false,
-	DEFAULT_LANGUAGE_CODE: 'en',
-	CALLBACK_JOIN: '#',
-	LANGUAGES_IN_ROW: 2,
+	DEFAULT_LANGUAGE_CODE: 'en'
 };
