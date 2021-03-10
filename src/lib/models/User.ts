153d34f00c5d7c2c96e87a9fc7d8f7fbdca33c6b
Harun Sokullu <harunsokullu@gmail.com>
2021-03-10 13:56:48 +0300

    add manual banned to user

--- src/lib/models/User.ts
+++ src/lib/models/User.ts
@@ -1,12 +1,14 @@
 import { Document, model, Model, Schema } from 'mongoose';
 
 export interface IUser extends Document {
+  banned?: boolean;
   chatId: number;
-  languageCode: string;
   blocked?: boolean;
+  languageCode: string;
 }
 
 const UserSchema: Schema = new Schema({
+  banned: { type: Boolean },
   blocked: { type: Boolean },
   chatId: { type: Number, required: true, unique: true },
   languageCode: { type: String, required: true },
