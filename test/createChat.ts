3919bc901a54d5a6fa9383c5797f1d3adf5baa5a
Harun Sokullu <harunsokullu@gmail.com>
2021-03-27 00:10:36 +0300

    fix dup

--- test/createChat.ts
+++ test/createChat.ts
@@ -5,7 +5,7 @@ import { IMessagineContext } from '../src/lib/common';
 import { DataHandler } from '../src/lib/dataHandler';
 import Chat, { IChat } from '../src/lib/models/Chat';
 import Lobby, { ILobby } from '../src/lib/models/Lobby';
-import PreviousChat from '../src/lib/models/PreviousChat';
+import PreviousChat, { IPreviousChat } from '../src/lib/models/PreviousChat';
 import { createChatMiddleware, dbMiddleware } from '../src/middlewares';
 import { fakeI18nMiddleware } from './fakes';
 
@@ -31,11 +31,21 @@ function createChat(chatIds: number[], languageCode: string): IChat {
   return chat;
 }
 
+function createPreviousChat(chatIds: number[], closedBy: number, languageCode: string): IPreviousChat {
+  const previousChat = new PreviousChat();
+  previousChat.chatIds = chatIds;
+  previousChat.closedBy = closedBy;
+  previousChat.languageCode = languageCode;
+  return previousChat;
+}
+
 let sandbox: sinon.SinonSandbox;
+let createChatStub: sinon.SinonStub;
 test.beforeEach(() => {
   sandbox = sinon.createSandbox();
   sandbox.stub(DataHandler.prototype, 'connect').resolves();
   sandbox.stub(Telegram.prototype, 'sendMessage').resolves();
+  createChatStub = sandbox.stub(DataHandler.prototype, 'createChat');
 });
 
 test.afterEach(() => {
@@ -44,18 +54,12 @@ test.afterEach(() => {
 
 test('handle empty lobby', async t => {
   sandbox.stub(DataHandler.prototype, 'getAllLobbyUsers').resolves(null);
-  const chat = createChat([1, 2], 'en');
-  const createChatStub = sandbox.stub(DataHandler.prototype, 'createChat');
-  createChatStub.resolves(chat);
 
   const bot = createBot();
-  bot.use(async (ctx, next) => {
-    await next();
-    t.assert('message' in ctx);
-    sinon.assert.notCalled(createChatStub);
-  });
-
   await bot.handleUpdate({ update_id: 0 });
+
+  t.is(1, 1);
+  sinon.assert.notCalled(createChatStub);
 });
 
 test('handle multiple users without chat lobby', async t => {
@@ -65,18 +69,12 @@ test('handle multiple users without chat lobby', async t => {
   const lobbies: ILobby[] = [lobby1, lobby2, lobby3];
   sandbox.stub(DataHandler.prototype, 'getAllLobbyUsers').resolves(lobbies);
   sandbox.stub(DataHandler.prototype, 'getUsersPreviousChats').resolves(null);
-  const chat = createChat([1, 2], 'en');
-  const createChatStub = sandbox.stub(DataHandler.prototype, 'createChat');
-  createChatStub.resolves(chat);
 
   const bot = createBot();
-  bot.use(async (ctx, next) => {
-    await next();
-    t.assert('message' in ctx);
-    sinon.assert.notCalled(createChatStub);
-  });
-
   await bot.handleUpdate({ update_id: 0 });
+
+  t.is(1, 1);
+  sinon.assert.notCalled(createChatStub);
 });
 
 test('handle users with chat', async t => {
@@ -88,18 +86,14 @@ test('handle users with chat', async t => {
   const leaveLobbyStub = sandbox.stub(DataHandler.prototype, 'leaveLobby');
   leaveLobbyStub.resolves(null);
   const chat = createChat([1, 2], 'en');
-  const createChatStub = sandbox.stub(DataHandler.prototype, 'createChat');
   createChatStub.resolves(chat);
 
   const bot = createBot();
-  bot.use(async (ctx, next) => {
-    await next();
-    t.assert('message' in ctx);
-    sinon.assert.calledOnce(createChatStub);
-    sinon.assert.calledTwice(leaveLobbyStub);
-  });
-
   await bot.handleUpdate({ update_id: 0 });
+
+  t.is(1, 1);
+  sinon.assert.calledOnce(createChatStub);
+  sinon.assert.calledTwice(leaveLobbyStub);
 });
 
 test('handle users with previous chat and chat', async t => {
@@ -108,26 +102,19 @@ test('handle users with previous chat and chat', async t => {
   const lobby3 = createLobby(3, 'en');
   const lobbies: ILobby[] = [lobby1, lobby2, lobby3];
   sandbox.stub(DataHandler.prototype, 'getAllLobbyUsers').resolves(lobbies);
-  const previousChat = new PreviousChat();
-  previousChat.chatIds = [1, 2];
-  previousChat.closedBy = 1;
-  previousChat.languageCode = 'en';
+  const previousChat = createPreviousChat([1, 2], 1, 'en');
   sandbox.stub(DataHandler.prototype, 'getUsersPreviousChats').resolves([previousChat]);
   const leaveLobbyStub = sandbox.stub(DataHandler.prototype, 'leaveLobby');
   leaveLobbyStub.resolves(null);
   const chat = createChat([1, 3], 'en');
-  const createChatStub = sandbox.stub(DataHandler.prototype, 'createChat');
   createChatStub.withArgs([1, 3], 'en').resolves(chat);
 
   const bot = createBot();
-  bot.use(async (ctx, next) => {
-    await next();
-    t.assert('message' in ctx);
-    sinon.assert.calledOnce(createChatStub);
-    sinon.assert.calledTwice(leaveLobbyStub);
-  });
-
   await bot.handleUpdate({ update_id: 0 });
+
+  t.is(1, 1);
+  sinon.assert.calledOnce(createChatStub);
+  sinon.assert.calledTwice(leaveLobbyStub);
 });
 
 test('handle users with multiple chats', async t => {
@@ -142,18 +129,14 @@ test('handle users with multiple chats', async t => {
   leaveLobbyStub.resolves(null);
   const enChat = createChat([1, 2], 'en');
   const deChat = createChat([3, 4], 'de');
-  const createChatStub = sandbox.stub(DataHandler.prototype, 'createChat');
   createChatStub.resolves(enChat).resolves(deChat);
 
   const bot = createBot();
-  bot.use(async (ctx, next) => {
-    await next();
-    t.assert('message' in ctx);
-    sinon.assert.calledTwice(createChatStub);
-    sinon.assert.callCount(leaveLobbyStub, 4);
-  });
-
   await bot.handleUpdate({ update_id: 0 });
+
+  t.is(1, 1);
+  sinon.assert.calledTwice(createChatStub);
+  sinon.assert.callCount(leaveLobbyStub, 4);
 });
 
 test('handle users with previous chat', async t => {
@@ -161,23 +144,14 @@ test('handle users with previous chat', async t => {
   const lobby2 = createLobby(2, 'en');
   const lobbies: ILobby[] = [lobby1, lobby2];
   sandbox.stub(DataHandler.prototype, 'getAllLobbyUsers').resolves(lobbies);
-  const previousChat = new PreviousChat();
-  previousChat.chatIds = [1, 2];
-  previousChat.closedBy = 1;
-  previousChat.languageCode = 'en';
+  const previousChat = createPreviousChat([1, 2], 1, 'en');
   sandbox.stub(DataHandler.prototype, 'getUsersPreviousChats').resolves([previousChat]);
   const leaveLobbyStub = sandbox.stub(DataHandler.prototype, 'leaveLobby');
   leaveLobbyStub.resolves(null);
-  const chat = createChat([1, 2], 'en');
-  const createChatStub = sandbox.stub(DataHandler.prototype, 'createChat');
-  createChatStub.resolves(chat);
 
   const bot = createBot();
-  bot.use(async (ctx, next) => {
-    await next();
-    t.assert('message' in ctx);
-    sinon.assert.notCalled(createChatStub);
-  });
-
   await bot.handleUpdate({ update_id: 0 });
+
+  t.is(1, 1);
+  sinon.assert.notCalled(createChatStub);
 });
